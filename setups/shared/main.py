e959bda52119c64dcf2b3d01bddc9dff87306df1
Felix <fmc@mail.uni-paderborn.de>
2024-08-05 18:03:38 +0200

    added java project

--- setups/shared/main.py
+++ setups/shared/main.py
@@ -1,11 +1,7 @@
-from lib.docker import Docker
-from lib.curl import Curl
-from lib.tls_attacker import TLS_Attacker
-from lib.openssl import OpenSSL #note this is not OpenSSL (builtin)
-from lib.setups import SETUPS
-
-request = TLS_Attacker.request
-requestWithCert = TLS_Attacker.requestWithCert
+from docker import Docker
+from curl import Curl
+from openssl import OpenSSL #note this is not OpenSSL (builtin)
+from tests import SETUPS
 
 REBUILD = False
 RESTART = True
@@ -26,8 +22,8 @@ from pathlib import Path
 paths = []
 servers = [
     Path("apache"),
-    #Path("caddy"),
-    #Path("nginx")
+    Path("caddy"),
+    Path("nginx")
 ]
 
 
@@ -35,77 +31,68 @@ def testSetup(setup, server, port):
     # build container
     path = Path(server).joinpath(setup.name)
     name = Docker.nameFromPath(path)
-    print(f"{bcolors.OKBLUE}# testing {name} on port {port}{bcolors.ENDC}")
+    print(f"\ntesting {name}")
     #pprint(setup)
 
     if not Docker.imageExists(name) or REBUILD:
         out, err, code = Docker.build(name, path)
         if not err=="":
             print(f"{bcolors.WARNING}[{code}] error while building image '{name}'{bcolors.ENDC}")
-            print(f"\t{bcolors.WARNING}'{err}'{bcolors.ENDC}")
-            return [name, "error during build"]
+            print(err)
 
     # start container
     out, err, code = Docker.run(imageName=name, containerName=name, restart=RESTART, port=port)
     sleep(0.5) #to wait for the server to start
     out, err, code = "", "", 0
 
-    # get SiteA correct
+    # get a correct
     if setup.aCert=="":
-        out, err, code = request(
+        out, err, code = Curl.request(
             target = Path(setup.aLocation), 
             caPath = Path(setup.caPath),
-            port=port,
-            name=f"{name}"
+            port=port
         )
     else:
-        out, err, code = requestWithCert(
+        out, err, code = Curl.requestWithCert(
             target = setup.aLocation,
             certPath = Path(f"{setup.aCert}.crt"),
             keyPath = Path(f"{setup.aCert}.key"),
             caPath = Path(setup.caPath),
-            port=port,
-            name=f"{name}_cert"
+            port=port
         )
     if err!="":
         print(f"{bcolors.FAIL}# ERROR: {err}{bcolors.ENDC}")
     else:
         print(f"{bcolors.OKGREEN}+ SUCCESS: {code}{bcolors.ENDC}")
 
-    # get SiteB correct
+    # get b correct
     if setup.bCert=="":
-        out2, err2, code2 = request(
+        out2, err2, code2 = Curl.request(
             target = Path(setup.bLocation), 
             caPath = Path(setup.caPath),
-            port=port,
-            name=f"{name}"
+            port=port
         )
     else:
-        out2, err2, code2 = requestWithCert(
+        out2, err2, code2 = Curl.requestWithCert(
             target = setup.bLocation,
             certPath = Path(f"{setup.bCert}.crt"),
             keyPath = Path(f"{setup.bCert}.key"),
             caPath = Path(setup.caPath),
-            port=port,
-            name=f"{name}_cert"
+            port=port
         )
     if err2!="":
         print(f"{bcolors.FAIL}# ERROR: {err2}{bcolors.ENDC}")
     else:
         print(f"{bcolors.OKGREEN}+ SUCCESS: {code2}{bcolors.ENDC}")
-    #print(f"output: {out}")
-    #print(f"output2: {out2}")
-    Docker.stop(name)
-    return [name, code, code2]
 
-p = Pool(1)
+    Docker.stop(name)
+    return [code, code2]
+
+p = Pool()
 c = []
 [(setup, server) for server in servers for setup in SETUPS]
 for setup in SETUPS:
     for server in servers:
         c.append([setup, server, 433+len(c)])
-try:
-    res = p.starmap(testSetup, c)
-    pprint(res)
-except KeyboardInterrupt:
-    print(f"{bcolors.WARNING}\nstopping{bcolors.ENDC}")
+res = p.starmap(testSetup, c)
+pprint(res)
