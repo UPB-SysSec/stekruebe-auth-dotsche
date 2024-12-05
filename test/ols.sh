d25949a87dcc93de518ee28a264513b451f0953f
Felix <fmc@mail.uni-paderborn.de>
2024-12-05 16:31:22 +0100

    removed the 'open' from openlitespeed

--- test/ols.sh
+++ test/ols.sh
@@ -1,4 +1,4 @@
 docker kill olsdomains
 docker run --rm -d --name olsdomains -p 4433:443 -p 7081:7080  openlitespeed_domains
 docker exec -it olsdomains sh -c "echo -n admin: > /usr/local/lsws/admin/conf/htpasswd"
-docker exec -it olsdomains sh -c "/usr/local/lsws/admin/fcgi-bin/admin_php -q /usr/local/lsws/admin/misc/htpasswd.php adminadmin >> /usr/local/lsws/admin/conf/htpasswd"
+docker exec -it olsdomains sh -c "/usr/local/lsws/admin/fcgi-bin/admin_php5 -q /usr/local/lsws/admin/misc/htpasswd.php adminadmin >> /usr/local/lsws/admin/conf/htpasswd"
