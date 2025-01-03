2abd7a4677d5db95c7305ae0e10e36521d47d19f
Tim Storm <tim.storm@gmx.net>
2025-01-03 22:42:17 +0100

    add missing nA testcase

---
+++ src/main/java/org/felix/thesis/testCases/Connect_A_nA.java
@@ -0,0 +1,29 @@
+package org.felix.thesis.testCases;
+
+import de.rub.nds.tlsattacker.core.config.Config;
+import de.rub.nds.tlsattacker.core.constants.ProtocolVersion;
+import de.rub.nds.tlsattacker.core.state.State;
+import de.rub.nds.tlsattacker.core.workflow.WorkflowTrace;
+import org.felix.thesis.BaseConfigCreator;
+import org.felix.thesis.BaseWorkflowCreator;
+import org.felix.thesis.TestOutcome;
+import org.felix.thesis.sessionTickets.Ticket;
+
+public class Connect_A_nA extends Connect_A_BB {
+    public Connect_A_nA(String name, ProtocolVersion version) {
+        super(name, version);
+        this.expectedTestOutcome = new TestOutcome[] {
+                TestOutcome.secondRequest_tlsAlert_unexpectedMessage,
+                TestOutcome.secondRequest_http421_misdirectedRequest
+        };
+    }
+
+    //getStateA is the same as for the base case
+
+    public State getStateB(Ticket ticket) {
+        Config config = BaseConfigCreator.buildConfig(port, siteBDomain, false, version); //no SNI
+        ticket.applyTo(config);
+        WorkflowTrace trace = BaseWorkflowCreator.getResumptionWorkflowTrace(config, siteADomain);
+        return new State(config, trace);
+    }
+}
