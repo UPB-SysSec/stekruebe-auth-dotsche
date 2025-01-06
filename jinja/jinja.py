27ee67a864e63eee72727eb22f0fa96165226324
Felix <fmc@mail.uni-paderborn.de>
2025-01-06 18:31:50 +0100

    removed isStrict

--- jinja/jinja.py
+++ jinja/jinja.py
@@ -1,14 +1,13 @@
 import os, shutil
-import jinja2
-from pprint import pprint
+from jinja2 import Template
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 StrictVariants = {
-    "apache" : ["default"],
-    "caddy" : ["default"],
-    "closedlitespeed" : ["default"],
-    "nginx" : ["HTTP", "TLS"]
+    "apache" : ["off", "default"],
+    "caddy" : ["off", "default"],
+    "closedlitespeed" : ["off"],
+    "nginx" : ["off", "HTTP", "TLS"]
 }
 
 PossibleDefaultSites = {
@@ -30,41 +29,38 @@ for d in os.listdir("."):
     if dName.startswith("_"): continue #skip folders like _keys etc.
 
     files = [f"{r}/{f}"[len(d)+3:] for r, sd, fn in os.walk(f"./{d}") for f in fn]
-    pprint(files)
     templates = []
     for file in files:
         with open(f"./{d}/{file}", "r", encoding="UTF-8") as f:
             print(f"reading template '{file}'")
-            templates.append([file, jinja2.Template(f.read())])
+            templates.append([file, Template(f.read())])
 
     for isSubdomain in [True,False]:
         for isCertA in [True,False]:
             for defaultSite in PossibleDefaultSites[dName]:
-                for isStrict in [True,False]:
-                    for strictType in (StrictVariants[dName] if isStrict else ["none"]):
-                        # build folder name
-                        folderName  = "subdomains" if isSubdomain else "domains"
-                        folderName += ("_certA" if isCertA else "")
-                        folderName += (f"_default{defaultSite[-1:]}" if defaultSite else "")
-                        folderName += (f"_strict_{strictType}" if isStrict else "")
+                for strictType in StrictVariants[dName]:
+                    # build folder name
+                    folderName  = "subdomains" if isSubdomain else "domains"
+                    folderName += ("_certA" if isCertA else "")
+                    folderName += (f"_default{defaultSite[-1:]}" if defaultSite else "")
+                    folderName += (f"_strict_{strictType}" if strictType!="off" else "")
 
-                        # build config
-                        for fileName, template in templates:
-                            fullName = f"{dName}/{folderName}/{fileName}"
-                            #print(f"building '{fullName}'")
-                            readyF = template.render(
-                                folderName =    folderName,
-                                isSubdomain =   isSubdomain,
-                                isCertA =       isCertA,
-                                defaultSite =   defaultSite,
-                                isStrict =      isStrict,
-                                strictType =    strictType
-                            )
-                            #print(f" > {readyF[:50].replace('\n','  ')}")
-                            # write config into folder
-                            if ".jinja" in fullName:
-                                fullName = fullName.split(".")[0]
-                            os.makedirs(os.path.dirname(f"../setups/{fullName}"), exist_ok=True)
-                            with open(f"../setups/{fullName}", "w+", encoding="UTF-8") as f:
-                                #print(f"Writing {fullName}")
-                                f.write(readyF)
+                    # build config
+                    for fileName, template in templates:
+                        fullName = f"{dName}/{folderName}/{fileName}"
+                        #print(f"building '{fullName}'")
+                        readyF = template.render(
+                            folderName =    folderName,
+                            isSubdomain =   isSubdomain,
+                            isCertA =       isCertA,
+                            defaultSite =   defaultSite,
+                            strictType =    strictType
+                        )
+                        #print(f" > {readyF[:50].replace('\n','  ')}")
+                        # write config into folder
+                        if ".jinja" in fullName:
+                            fullName = fullName.split(".")[0]
+                        os.makedirs(os.path.dirname(f"../setups/{fullName}"), exist_ok=True)
+                        with open(f"../setups/{fullName}", "w+", encoding="UTF-8") as f:
+                            #print(f"Writing {fullName}")
+                            f.write(readyF)
